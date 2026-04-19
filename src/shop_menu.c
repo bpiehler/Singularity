c3a587e8edca918ff8d063647dbb1dacd28c42dc
Britt Piehler <brittp@gmail.com>
2026-04-18 20:15:58 -0700

    Diagnostic: Implement atomic logging in shop_menu to isolate exact crash line

--- src/shop_menu.c
+++ src/shop_menu.c
@@ -8,7 +8,7 @@ static GameState *s_game_state;
 static ShopPurchaseCallback s_callback;
 
 static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
-  return NUM_TIERS + 1; // 9 Tiers + Big Bang
+  return NUM_TIERS + 1;
 }
 
 static int16_t menu_get_cell_height_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
@@ -17,36 +17,38 @@ static int16_t menu_get_cell_height_callback(MenuLayer *menu_layer, MenuIndex *c
 
 static void menu_draw_row_callback(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
   int i = cell_index->row;
-  if (!s_game_state || i > NUM_TIERS) return;
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Shop: Draw Row Index");
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "%d", i);
 
-  // Static buffers to save stack space (Pebble UI thread has < 4KB)
+  if (!s_game_state) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Shop: GameState is NULL");
+    return;
+  }
+
   static char s_name_buf[64];
   static char s_cost_buf[64];
   static char s_val_buf[32];
   bool affordable = false;
 
   if (i < NUM_TIERS) {
-    // Normal Tier
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Shop: Accessing Tier");
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", TIERS[i].name);
+
     double cost = calculate_cost(TIERS[i].base_cost, s_game_state->counts[i]);
     affordable = s_game_state->mass >= cost;
-    snprintf(s_name_buf, sizeof(s_name_buf), "%s (x%d)", TIERS[i].name, s_game_state->counts[i]);
     
+    snprintf(s_name_buf, sizeof(s_name_buf), "%s (x%d)", TIERS[i].name, s_game_state->counts[i]);
     format_mass(cost, s_val_buf);
     snprintf(s_cost_buf, sizeof(s_cost_buf), "Cost: %s", s_val_buf);
   } else {
-    // The Big Bang (10th Row)
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Shop: Drawing Big Bang");
     affordable = s_game_state->mass >= PRESTIGE_THRESHOLD;
     snprintf(s_name_buf, sizeof(s_name_buf), "%s", affordable ? "THE BIG BANG" : "SINGULARITY");
     
     if (affordable) {
       double dust = calculate_prestige_dust(s_game_state->mass, PRESTIGE_THRESHOLD);
-      // Simplify display to avoid modf/casts in the draw loop
       int d_int = (int)dust;
-      if (d_int < 1000000) {
-        snprintf(s_cost_buf, sizeof(s_cost_buf), "Reward: %d Dust", d_int);
-      } else {
-        snprintf(s_cost_buf, sizeof(s_cost_buf), "Reward: >1M Dust");
-      }
+      snprintf(s_cost_buf, sizeof(s_cost_buf), "Reward: %d Dust", d_int);
     } else {
       format_mass(PRESTIGE_THRESHOLD, s_val_buf);
       snprintf(s_cost_buf, sizeof(s_cost_buf), "Goal: %s", s_val_buf);
@@ -82,6 +84,9 @@ static void menu_draw_row_callback(GContext *ctx, const Layer *cell_layer, MenuI
   graphics_draw_text(ctx, s_cost_buf, fonts_get_system_font(FONT_KEY_GOTHIC_18), 
                      GRect(left_padding, 27, bounds.size.w - (left_padding + 5), 20), 
                      GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
+  
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Shop: Done Row");
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "%d", i);
 }
 
 static void big_bang_timer_callback(void *data) {
@@ -90,7 +95,6 @@ static void big_bang_timer_callback(void *data) {
 
 static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
   int i = cell_index->row;
-  
   if (i < NUM_TIERS) {
     double cost = calculate_cost(TIERS[i].base_cost, s_game_state->counts[i]);
     if (s_game_state->mass >= cost) {
@@ -117,10 +121,8 @@ static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, v
 static void menu_select_long_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
   int i = cell_index->row;
   if (i >= NUM_TIERS) return;
-
   double start_mass = s_game_state->mass;
   game_state_buy_max(s_game_state, i);
-  
   if (s_game_state->mass < start_mass) {
     vibes_long_pulse();
     menu_layer_reload_data(s_menu_layer);
@@ -129,9 +131,9 @@ static void menu_select_long_callback(MenuLayer *menu_layer, MenuIndex *cell_ind
 }
 
 static void shop_window_load(Window *window) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Shop: Load Start");
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-
   window_set_background_color(window, GColorBlack);
 
   s_menu_layer = menu_layer_create(bounds);
@@ -152,16 +154,25 @@ static void shop_window_load(Window *window) {
 
   menu_layer_set_click_config_onto_window(s_menu_layer, window);
   
-  // Smart Selection
+  // Smart Selection Loop
+  APP_LOG(APP_LOG_LEVEL_INFO, "Shop: Smart Select Start");
   int initial_row = 0;
   for (int i = NUM_TIERS - 1; i >= 0; i--) {
-    if (s_game_state->mass >= calculate_cost(TIERS[i].base_cost, s_game_state->counts[i])) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Shop: Checking Tier");
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "%d", i);
+    
+    double cost = calculate_cost(TIERS[i].base_cost, s_game_state->counts[i]);
+    if (s_game_state->mass >= cost) {
       initial_row = i;
       break;
     }
   }
+  APP_LOG(APP_LOG_LEVEL_INFO, "Shop: Initial Row Chosen");
+  APP_LOG(APP_LOG_LEVEL_INFO, "%d", initial_row);
+
   menu_layer_set_selected_index(s_menu_layer, MenuIndex(0, initial_row), MenuRowAlignCenter, false);
   layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
+  APP_LOG(APP_LOG_LEVEL_INFO, "Shop: Load Complete");
 }
 
 static void shop_window_unload(Window *window) {
@@ -173,14 +184,13 @@ static void shop_window_unload(Window *window) {
 }
 
 void shop_menu_show(GameState *state, ShopPurchaseCallback callback) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Shop: Show Called");
   s_game_state = state;
   s_callback = callback;
-  
   if (s_shop_window) {
     window_stack_push(s_shop_window, true);
     return;
   }
-  
   s_shop_window = window_create();
   window_set_window_handlers(s_shop_window, (WindowHandlers) {
     .load = shop_window_load,
@@ -190,9 +200,7 @@ void shop_menu_show(GameState *state, ShopPurchaseCallback callback) {
 }
 
 void shop_menu_hide() {
-  if (s_shop_window) {
-    window_stack_pop(true);
-  }
+  if (s_shop_window) window_stack_pop(true);
 }
 
 void shop_menu_deinit() {
